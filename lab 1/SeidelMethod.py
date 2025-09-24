beeef7e7eb75f313968d5635cede5d2b62458b94
anxiousss <saimonkheresh@gmail.com>
2025-09-24 19:22:58 +0300

    Seidel method done

--- lab 1/SeidelMethod.py
+++ lab 1/SeidelMethod.py
@@ -2,7 +2,8 @@ from typing import List
 
 from utility.matrix import Matrix
 
-def simple_iteration_method(system: Matrix, accuracy: float) -> List[int | float]:
+
+def seidel_method(system: Matrix, accuracy: float) -> List[int | float]:
     if system.calc_determinant() == 0 or system.matrix[0][0] == 0:
         raise ValueError('Разложение не работает для вырожденных матриц.')
 
@@ -10,16 +11,16 @@ def simple_iteration_method(system: Matrix, accuracy: float) -> List[int | float
     alpha.matrix = [[0 if i == j else -system.matrix[i][j] / system.matrix[i][i]
                      for j in range(alpha.columns)] for i in range(alpha.rows)]
     beta = [system.free_members[i] / system.matrix[i][i] for i in range(system.rows)]
-    X_prev, X_next = beta.copy(), beta.copy()
+    X_prev, X_next = beta.copy(), []
     diff = False
     while not diff:
         for i in range(system.rows):
-            X_next[i] = sum(alpha.matrix[i][l] * X_prev[l] if l != i else 0
-                            for l in range(alpha.columns)) + beta[i]
+            el = (sum(alpha.matrix[i][j] * X_next[j] if i != j else 0 for j in range(len(X_next)))
+                  + sum(alpha.matrix[i][k] * X_prev[k] for k in range(len(X_next), alpha.columns))) + beta[i]
+            X_next.append(el)
         diff = all(abs(X_next[i] - X_prev[i]) < accuracy for i in range(system.rows))
         X_prev = X_next
-        X_next = [0 for _ in range(system.rows)]
-
+        X_next = []
 
     return X_prev
 
@@ -28,7 +29,7 @@ def main():
                            [4, 28, 7, 9],
                            [6, 5, -23, 4],
                            [1, 4, 5, -15]], [-99, 0, 67, 58])
-    print(simple_iteration_method(system, 1e-20))
+    print(seidel_method(system, 1e-20))
 
 
 if __name__ == '__main__':
